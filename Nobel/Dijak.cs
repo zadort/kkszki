99a3ac1aa006ddb116f125077a0d9820651d8bea
zadort <127024072+zadort@users.noreply.github.com>
2024-09-12 08:35:43 +0200

    Add files via upload

---
+++ Nobel/Dijak.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel
+{
+    class Dij
+    {
+        public int ev;
+        public string tipus;
+        public string keresztnev;
+        public string vezeteknev;
+
+        public Dij(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            this.ev = Convert.ToInt32(darabok[0]);
+            this.tipus = darabok[1];
+            this.keresztnev = darabok[2];
+            this.vezeteknev = darabok[3];
+        }
+    }
+}
---
+++ Nobel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dij> dijlista = new List<Dij>();
+            var sorok = File.ReadAllLines("nobel.csv").Skip(1);
+
+            foreach (var sor in sorok)
+            {
+                dijlista.Add(new Dij(sor));
+            }
+
+            foreach (var item in dijlista)
+            {
+                Console.WriteLine($"{item.ev} {item.tipus} {item.keresztnev} {item.vezeteknev}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
