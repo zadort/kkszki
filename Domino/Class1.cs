fbc6c4dcf28102f7696341ba58157a23b5e87da0
zadort <127024072+zadort@users.noreply.github.com>
2024-09-19 11:54:04 +0200

    Add files via upload

---
+++ Domino/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domino
+{
+    class Class1
+    {
+        public int szam1;
+        public int szam2;
+
+        public Class1(string sor)
+        {
+            string[] darabok =sor.Split(' ');
+            this.szam1 = Convert.ToInt32(darabok[0]);
+            this.szam2 = Convert.ToInt32(darabok[1]);
+        }
+    }
+}
