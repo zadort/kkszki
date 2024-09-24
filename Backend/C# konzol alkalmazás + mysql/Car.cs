1cc4657257e118e91ebd0bd25c4597a6ba8808c0
zadort <127024072+zadort@users.noreply.github.com>
2024-09-24 14:29:06 +0200

    Add files via upload

---
+++ Backend/C# konzol alkalmazás + mysql/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto.Model
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string License { get; set; }
+        public int Date { get; set; }
+    }
+}
