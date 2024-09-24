3b6a32c161955e224569e1a10ac04044a8164dfe
zadort <127024072+zadort@users.noreply.github.com>
2024-09-24 14:22:30 +0200

    Add files via upload

---
+++ Backend/C# konzol alkalmazás + mysql/Connect.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Auto
+{
+    public class Connect
+    {
+        public MySqlConnection Connection;
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "auto";
+            Username = "root";
+            Password = "";
+            ConnectionString = ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ Backend/C# konzol alkalmazás + mysql/Program.cs
@@ -0,0 +1,72 @@
+﻿using Auto.Model;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using MySqlX.XDevAPI.CRUD;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
+
+namespace Auto
+{
+    internal class Program
+    {
+        public static Connect conn = new Connect();
+        public static List<Car> cars = new List<Car>();
+
+        static void feltolt()
+        {
+            conn.Connection.Open();
+            string sql = "SELECT * FROM `cars`";
+            MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            do
+            {
+                Car car = new Car();
+                reader.Read();
+                car.Id = reader.GetInt32(0);
+                car.Brand = reader.GetString(1);
+                car.Type = reader.GetString(2);
+                car.License = reader.GetString(3);
+                car.Date = reader.GetInt32(4);
+                cars.Add(car);
+            } while (reader.Read());
+            conn.Connection.Close();
+        }
+
+        #region 2. feladat
+        //2. feladat
+        public static void addNewCar()
+        {
+            conn.Connection.Open();
+            string brand, type, license;
+            int date;
+            Console.WriteLine("Kérem az autó márkját: ");
+            brand = Console.ReadLine();
+            Console.Write("Kérem az autó típusát: ");
+            type = Console.ReadLine();
+            Console.WriteLine("Kérem az autó motorszámát:  ");
+            license = Console.ReadLine();
+            Console.WriteLine("Kérem az autó gyártási évét: ");
+            date = int.Parse(Console.ReadLine());
+            string sql = $"INSERT INTO `cars`(`Id`, `Brand`, `Type`, `License`, `Date`) VALUES('{brand}', '{type}', '{license}', '{date}')";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.ExecuteNonQuery();
+            conn.Connection.Close();
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            feltolt();
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"Autó gyártója: {item.Brand}, motorszáma: {item.License}");
+            }
+            addNewCar();
+
+            Console.ReadLine();
+        }
+    }
+}
